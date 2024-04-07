8bf3f4a705d13f148b034439645f648f7f04e8d9
mohammed a alghamdi <155687340+ghleprogramer@users.noreply.github.com>
2024-04-08 00:46:38 +0300

    Add files via upload

---
+++ fractal_functions.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+#include "stolen_bmp.h"
+
+const int mandelbrot_EscapeValue = 2;
+const int burningship_EscapeValue = 40;
+
+BYTE mandelbrot(float x, float iy, int maxlength)
+{    
+    float ic[] = {x, iy};
+    float z[] = {x, iy};
+    for (int i = 1; i < maxlength; i++)
+    {
+        if (pow((z[0]*z[0] + z[1]*z[1]), 0.5) > mandelbrot_EscapeValue)
+        {
+            return i;
+        }
+        float temp = z[0]*z[0] - z[1]*z[1] + ic[0];
+        z[1] = 2 * z[0] * z[1] + ic[1];
+        z[0] = temp;
+    }
+    return 0;
+}
+
+BYTE burningship(float x, float iy, int maxlength)
+{
+    float ic[] = {x, iy};
+    float z[] = {x, iy};
+    for (int i = 1; i < maxlength; i++)
+    {
+        if (pow((z[0]*z[0] + z[1]*z[1]), 0.5) > burningship_EscapeValue)
+        {
+            return i;
+        }
+        z[0] = fabsf(z[0]);
+        z[1] = fabsf(z[1]);
+        float temp = z[0]*z[0] - z[1]*z[1] + ic[0];
+        z[1] = 2 * z[0] * z[1] + ic[1];
+        z[0] = temp;
+    }
+    return 0;    
+}
