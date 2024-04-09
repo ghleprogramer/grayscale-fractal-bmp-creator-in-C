e4adf8bbe9d82840ee73ccad32f855636ab856bd
mohammed a alghamdi <155687340+ghleprogramer@users.noreply.github.com>
2024-04-09 04:14:23 +0300

    Delete fractal_functions.h

--- fractal_functions.h
+++
@@ -1,5 +0,0 @@
-#include "stolen_bmp.h"
-BYTE mandelbrot(float x, float iy, int maxlength);
-BYTE burningship(float x, float iy, int maxlength);
-const int mandelbrot_EscapeValue = 2;
-const int burningship_EscapeValue = 60;
